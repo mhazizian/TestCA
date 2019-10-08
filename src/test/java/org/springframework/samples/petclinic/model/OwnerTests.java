73f602eb2d1b46a4f654c8304fe176c61c123db0
mohammad hosein <mohammadhosein@mohammads-MacBook-Pro-2.local>
2019-10-09 00:27:24 +0330

    Add tests for getPetsInternal and setPetsInternal methods

--- src/test/java/org/springframework/samples/petclinic/model/OwnerTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/OwnerTests.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Locale;
 import java.util.Set;
+import java.util.HashSet;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
@@ -18,6 +19,8 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
 
 
 public class OwnerTests {
@@ -62,7 +65,6 @@ public class OwnerTests {
 
     @Before
     public void setUp(){
-
     }
 
     @Test
@@ -117,6 +119,67 @@ public class OwnerTests {
 
         owner.setTelephone(testTelephone);
         assertEquals( (String) getField(owner, "telephone"), testTelephone);
+    }
+
+    @Test
+    public void getPetsInternalNullTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Owner owner = new Owner();
+        setField(owner, "pets", null);
+
+        Method method = Owner.class.getDeclaredMethod("getPetsInternal", null);
+
+        method.setAccessible(true);
+        Set<Pet> returnValue = (Set<Pet>) method.invoke(owner, null);
+        method.setAccessible(false);
+
+        assertTrue(returnValue instanceof HashSet<?>);
+        assertEquals(returnValue.size(), 0);
+    }
+
+    @Test
+    public void getPetsInternalWithValueTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Owner owner = new Owner();
+        
+        HashSet<Pet> pets = new HashSet<>();
+        Pet pet = new Pet();
+        Pet pet2 = new Pet();
+        pets.add(pet);
+        setField(owner, "pets", pets);
+
+        Method method = Owner.class.getDeclaredMethod("getPetsInternal", null);
+
+        method.setAccessible(true);
+        Set<Pet> returnValue = (Set<Pet>) method.invoke(owner, null);
+        method.setAccessible(false);
+
+        assertTrue(returnValue instanceof HashSet<?>);
+        assertEquals(returnValue.size(), 1);
+        assertTrue(returnValue.contains(pet));
+        assertFalse(returnValue.contains(pet2));
+    }
+
+    @Test
+    public void setPetsInternalTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Owner owner = new Owner();
+
+        HashSet<Pet> pets = new HashSet<>();
+        Pet pet = new Pet();
+        Pet pet2 = new Pet();
+        pets.add(pet);
+        pets.add(pet2);
+
+        Method method = Owner.class.getDeclaredMethod("setPetsInternal", Set.class);
+
+        method.setAccessible(true);
+        Set<Pet> returnValue = (Set<Pet>) method.invoke(owner, pets);
+        method.setAccessible(false);
+
+        HashSet<Pet> settedPets = (HashSet) getField(owner, "pets");
+
+        assertTrue(settedPets instanceof HashSet<?>);
+        assertEquals(settedPets.size(), 2);
+        assertTrue(settedPets.contains(pet));
+        assertTrue(settedPets.contains(pet2));
     }
 
     @Test
