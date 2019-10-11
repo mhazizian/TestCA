8100e9cc8d4fdb40186cdb60d69bb041f5006db0
amir <iamirranjbar@gmail.com>
2019-10-11 23:22:18 +0330

    Add theory test.

--- src/test/java/org/springframework/samples/petclinic/model/TheoryTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/TheoryTests.java
@@ -10,7 +10,10 @@ import java.util.*;
 
 @RunWith(Theories.class)
 public class TheoryTests {
-    Owner owner;
+    private Owner owner;
+    public ArrayList<String> oldPets;
+    public ArrayList<String> newPets;
+
     @Before
     public void setUp(){
         this.owner = new Owner();
@@ -19,19 +22,23 @@ public class TheoryTests {
         petA.setName("Dog");
         petA.setId(10000);
         owner.addPet(petA);
+        this.oldPets.add("Dog");
 
         Pet petB = new Pet();
         petB.setName("rabbit");
         petB.setId(10001);
         owner.addPet(petB);
+        this.oldPets.add("rabbit");
 
         Pet petC = new Pet();
         petC.setName("kitty");
         owner.addPet(petC);
+        this.newPets.add("kitty");
 
         Pet petD = new Pet();
         petD.setName("Hamster");
         owner.addPet(petD);
+        this.newPets.add("Hamster");
     }
     
     @DataPoints
@@ -43,5 +50,14 @@ public class TheoryTests {
     @Theory
     public void getPetWithNameAndIgnoreTest(String name, boolean ignoreNew) {
         Pet foundPet = this.owner.getPet(name, ignoreNew);
+        name = name.toLowerCase();
+        if (this.oldPets.contains(name)){
+            assertTrue(name + "has been found.", foundPet!=null);
+        } else if (this.newPets.contains(name)) {
+            if (ignoreNew)
+                assertTrue("New pet should not be found.", foundPet==null);
+            else
+                assertTrue(name + "has been found with false flag.", foundPet!=null);
+        } else assertTrue("This pet should not found.", foundPet==null);
     }
 }
