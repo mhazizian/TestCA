6eab3ced6163b4cb10a9a3af8e4740e1c4472ee1
amir <iamirranjbar@gmail.com>
2019-10-11 23:57:46 +0330

    Complete CA 1.

--- src/test/java/org/springframework/samples/petclinic/model/TheoryTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/TheoryTests.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.model;
 
 import org.junit.*;
+import org.junit.experimental.theories.DataPoint;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
@@ -10,35 +11,35 @@ import java.util.*;
 
 @RunWith(Theories.class)
 public class TheoryTests {
-    private Owner owner;
-    public ArrayList<String> oldPets;
-    public ArrayList<String> newPets;
+    public ArrayList<String> oldPets = new ArrayList<>(
+        Arrays.asList("dog", "rabbit")
+    );
+    public ArrayList<String> newPets = new ArrayList<>(
+        Arrays.asList("kitty", "hamster")
+    );
 
-    @Before
-    public void setUp(){
-        this.owner = new Owner();
+    @DataPoint
+    public static Owner getOwner(){
+        Owner owner = new Owner();
 
         Pet petA = new Pet();
         petA.setName("Dog");
         petA.setId(10000);
         owner.addPet(petA);
-        this.oldPets.add("Dog");
 
         Pet petB = new Pet();
         petB.setName("rabbit");
         petB.setId(10001);
         owner.addPet(petB);
-        this.oldPets.add("rabbit");
 
         Pet petC = new Pet();
         petC.setName("kitty");
         owner.addPet(petC);
-        this.newPets.add("kitty");
 
         Pet petD = new Pet();
         petD.setName("Hamster");
         owner.addPet(petD);
-        this.newPets.add("Hamster");
+        return owner;
     }
     
     @DataPoints
@@ -48,8 +49,9 @@ public class TheoryTests {
     public static boolean[] ignoreFlags = {true, false};
 
     @Theory
-    public void getPetWithNameAndIgnoreTest(String name, boolean ignoreNew) {
-        Pet foundPet = this.owner.getPet(name, ignoreNew);
+    public void getPetWithNameAndIgnoreTest(Owner owner, String name, boolean ignoreNew) {
+
+        Pet foundPet = owner.getPet(name, ignoreNew);
         name = name.toLowerCase();
         if (this.oldPets.contains(name)){
             assertTrue(name + "has been found.", foundPet!=null);
