bf99df2849240118703966b82535b68dba02ce25
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-10-26 15:57:24 +0330

    Add userWithNullRole test

--- src/test/java/org/springframework/samples/petclinic/service/userService/UserServiceImplBehavioralTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/userService/UserServiceImplBehavioralTest.java
@@ -1,15 +1,17 @@
 package org.springframework.samples.petclinic.service.userService;
 
-
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.Mockito;
-
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Mockito.*;
 
 import org.springframework.samples.petclinic.model.User;
 import org.springframework.samples.petclinic.repository.UserRepository;
@@ -17,6 +19,9 @@ import org.springframework.samples.petclinic.service.UserServiceImpl;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplBehavioralTest {
+
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
 
 	@Mock
 	UserRepository userRepository;
@@ -31,9 +36,26 @@ public class UserServiceImplBehavioralTest {
 	public void setup() {
     //    Mockito.when(userRepository.save(user)).do
 	}
+
+	@Test
+	public void userWithNullRole() throws Exception {
+		expectedEx.expect(Exception.class);
+		expectedEx.expectMessage("User must have at least a role set!");
+
+		when(user.getRoles()).thenReturn(null);
+		
+		userServiceImpl.saveUser(user);
+		
+		verify(user, times(1)).getRoles();
+	}
 	
 	@Test
 	public void shouldsaveAUser() {
         
+// 	when(userRepository.save(any(User.class))).thenAnswer(i -> {
+//     User user = i.getArgument(0);
+//     userMap.add(user.getId(), user);
+//     return null;
+// });
 	}
 }
