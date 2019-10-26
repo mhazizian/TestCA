3626f38a7c36bc14b63e257bcfe4323f911a1684
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-10-26 18:32:32 +0330

    Adding standardizeUserRoleTest Tests

--- src/test/java/org/springframework/samples/petclinic/service/userService/UserServiceImplBehavioralTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/userService/UserServiceImplBehavioralTest.java
@@ -1,19 +1,20 @@
 package org.springframework.samples.petclinic.service.userService;
 
+import java.util.HashSet;
+
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.Mockito;
-import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Mockito.*;
 
 import org.springframework.samples.petclinic.model.User;
+import org.springframework.samples.petclinic.model.Role;
 import org.springframework.samples.petclinic.repository.UserRepository;
 import org.springframework.samples.petclinic.service.UserServiceImpl;
 
@@ -34,7 +35,6 @@ public class UserServiceImplBehavioralTest {
 
 	@Before
 	public void setup() {
-    //    Mockito.when(userRepository.save(user)).do
 	}
 
 	@Test
@@ -47,6 +47,55 @@ public class UserServiceImplBehavioralTest {
 		userServiceImpl.saveUser(user);
 		
 		verify(user, times(1)).getRoles();
+	}
+
+	@Test
+	public void userWithEmptyRole() throws Exception {
+		expectedEx.expect(Exception.class);
+		expectedEx.expectMessage("User must have at least a role set!");
+
+		when(user.getRoles()).thenReturn(new HashSet<>());
+		
+		userServiceImpl.saveUser(user);
+		
+		verify(user, times(1)).getRoles();
+	}
+
+	@Test
+	public void standardizeUserRoleTest() throws Exception {
+		Role role1 = mock(Role.class);
+		Role role2 = mock(Role.class);
+
+		when(role1.getName()).thenReturn("roleName");
+		when(role2.getName()).thenReturn("ROLE_role2");
+
+
+
+	// 	when(role1.setName(any(String.class))).thenAnswer(i -> {
+    //     	String roleName = i.getArgument(0);
+    //     	assertEquals("ROLE_roleName", roleName);
+    //     	return null;
+    // });
+
+		HashSet<Role> roles = new HashSet<>();
+		roles.add(role1);
+		roles.add(role2);
+
+		when(user.getRoles()).thenReturn(roles);
+
+		// when(role2.getUser()).thenReturn(null);
+		userServiceImpl.saveUser(user);
+
+
+		verify(role1, times(1)).setName(any(String.class));
+		verify(role2, times(0)).setName(any(String.class));
+
+		// ArgumentCaptor<String> roleNameCaptor = ArgumentCaptor.forClass(String.class);
+		// verify(role1, times(1)).doSomething(captor.capture());
+
+		// // Assert the argument
+		// SomeData actual = captor.getValue();
+		// assertEquals("Some inner data", actual.innerData);
 	}
 	
 	@Test
