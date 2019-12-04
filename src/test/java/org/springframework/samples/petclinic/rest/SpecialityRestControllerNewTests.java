bbd48983d0aea096c7149795c72a12ac52e3f74b
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-12-04 16:17:14 +0330

    reafactor

--- src/test/java/org/springframework/samples/petclinic/rest/SpecialityRestControllerNewTests.java
+++ src/test/java/org/springframework/samples/petclinic/rest/SpecialityRestControllerNewTests.java
@@ -6,32 +6,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.model.Specialty;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.samples.petclinic.service.clinicService.ApplicationTestConfig;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@SpringBootTest
+
+@RunWith(SpringRunner.class)
 @WebMvcTest(SpecialtyRestController.class)
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes= ApplicationTestConfig.class)
-@WebAppConfiguration
 public class SpecialityRestControllerNewTests {
     @Autowired
     private SpecialtyRestController specialtyRestController;
@@ -40,8 +34,6 @@ public class SpecialityRestControllerNewTests {
     private ClinicService clinicService;
 
     private MockMvc mockMvc;
-
-    private List<Specialty> specialties;
 
     @Before
     public void initSpecialtys(){
